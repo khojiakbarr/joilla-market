871e4498f5a05050523e3d6c807e71016c3d053d
khojiakbarr <chinozbekov09@gmail.com>
2024-02-25 15:29:29 +0500

    new version

--- js/main.js
+++ js/main.js
@@ -15,9 +15,9 @@ adminbtn.addEventListener("click", () => {
   employee.classList.add("hidden");
   employee.classList.add("hidden");
 
-  adminbtn.classList.add('bg_white')
-  employeebtn.classList.remove('bg_white')
-  employerbtn.classList.remove('bg_white')
+  adminbtn.classList.add("bg_white");
+  employeebtn.classList.remove("bg_white");
+  employerbtn.classList.remove("bg_white");
 });
 
 employeebtn.addEventListener("click", () => {
@@ -25,18 +25,19 @@ employeebtn.addEventListener("click", () => {
   admin.classList.add("hidden");
   employer.classList.add("hidden");
 
-  employeebtn.classList.add('bg_white')
-  adminbtn.classList.remove('bg_white')
-  employerbtn.classList.remove('bg_white')
+  employeebtn.classList.add("bg_white");
+  adminbtn.classList.remove("bg_white");
+  employerbtn.classList.remove("bg_white");
 });
+
 employerbtn.addEventListener("click", () => {
   employer.classList.remove("hidden");
   admin.classList.add("hidden");
   employee.classList.add("hidden");
 
-  employerbtn.classList.add('bg_white')
-  adminbtn.classList.remove('bg_white')
-  employeebtn.classList.remove('bg_white')
+  employerbtn.classList.add("bg_white");
+  adminbtn.classList.remove("bg_white");
+  employeebtn.classList.remove("bg_white");
 });
 
 //? Forms
@@ -48,3 +49,56 @@ const employeeFormReg = document.querySelector("#employeeFormReg");
 
 const employerFormLog = document.querySelector("#employerFormLog");
 const employerFormReg = document.querySelector("#employerFormReg");
+
+//? Login Register toggle Form
+const loginToggle = document.querySelector("#loginToggle");
+const RegisterToggle = document.querySelector("#RegisterToggle");
+//? Admin
+loginToggle.addEventListener("click", () => {
+  adminFormReg.classList.add("hidden");
+  adminFormLog.classList.remove("hidden");
+
+  loginToggle.classList.add("hidden");
+  RegisterToggle.classList.remove("hidden");
+});
+RegisterToggle.addEventListener("click", () => {
+  adminFormLog.classList.add("hidden");
+  adminFormReg.classList.remove("hidden");
+
+  loginToggle.classList.remove("hidden");
+  RegisterToggle.classList.add("hidden");
+});
+
+//? employeeFormLog
+loginToggle.addEventListener("click", () => {
+  employeeFormReg.classList.add("hidden");
+  employeeFormLog.classList.remove("hidden");
+
+  loginToggle.classList.add("hidden");
+  RegisterToggle.classList.remove("hidden");
+});
+
+RegisterToggle.addEventListener("click", () => {
+  employeeFormLog.classList.add("hidden");
+  employeeFormReg.classList.remove("hidden");
+
+  loginToggle.classList.remove("hidden");
+  RegisterToggle.classList.add("hidden");
+});
+
+//? employerFormLog
+loginToggle.addEventListener("click", () => {
+  employerFormReg.classList.add("hidden");
+  employerFormLog.classList.remove("hidden");
+
+  loginToggle.classList.add("hidden");
+  RegisterToggle.classList.remove("hidden");
+});
+
+RegisterToggle.addEventListener("click", () => {
+  employerFormLog.classList.add("hidden");
+  employerFormReg.classList.remove("hidden");
+
+  loginToggle.classList.remove("hidden");
+  RegisterToggle.classList.add("hidden");
+});
